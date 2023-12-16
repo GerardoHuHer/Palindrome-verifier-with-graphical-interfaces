35ccd911e84732a74ea7c4c1cf91a428777f02e6
Gerardo Huerta Hernández <gerardohuerta1502@gmail.com>
2023-12-15 23:44:45 -0600

    Primer actualización de los archivos

---
+++ graphic_interface.py
@@ -0,0 +1,10 @@
+import tkinter as tk 
+from main import sentence
+
+class interface(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+
+app = interface()
+app.mainloop()
---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+    This project will verify if a string is a palindrome or not
+"""
+class sentence:
+    def __init__(self, sentence = " "):
+        self.__sentence = sentence.upper()
+
+    def verifier(self):
+        aux_sentence = self.__sentence.replace(" ", "")
+        reverse_word = aux_sentence[::-1]
+        if aux_sentence == reverse_word:
+            return True
+        else:
+            return False
+
+word = sentence("anita lava la tina")
+print(word.verifier())
